f0db3d37f774c2a6d78a066915cfc3b63fc89bbf
unknown <zubairwastibaloch@gmail.com>
2024-01-20 14:15:26 +0500

    new misalignment fixed

--- src/Component/webGaming.js
+++ src/Component/webGaming.js
@@ -10,15 +10,15 @@ const webGaming = () => {
     </div>
     <div className='sm:flex  sm:flex-wrap sm:gap-6 sm-mt-20 md:flex !md:flex-wrap md:gap-6 md:mt-20'>
     
-        <div className='mt-8  md:mt-0 md:flex xs:w-[330px] xs:h-[160px] cursor-pointer xs:pb-1 xs:pl-2  flex-col pb-8 justify-center rounded-[24px] bg-[#272727] hover:bg-[#040405] md:h-[160px]
-          md:gap-2 xl:h-[170px] xl:w-[17.1875vw] xl:gap-3 2xl:h-[210px] 2xl:gap-4 2xxl:h-[220px]'>
+        <div className='mt-8  md:mt-0 md:flex xs: pb-2 xs:w-[330px]  xs:h-[160px] cursor-pointer  xs:pl-2  flex-col justify-center rounded-[24px] bg-[#272727] hover:bg-[#040405] md:h-[160px]
+          md:gap-2 xl:h-[170px] xl:w-[17.1875vw] xs:pt-[0px] xl:gap-3 2xl:h-[210px] 2xl:gap-4 2xxl:h-[220px]'>
             {/* <img src='assets/Web3/igo.20418964.svg' className='sm:hidden'/> */}
             <div className=' sm:block '>
             <img src='/assets/Web3/ido.802696b8.svg' className='md:w-[18%] xs:ml-4 mt-8 md:ml-3'/>
             <div className='flex xs:ml-4 gap-2 md:ml-4 md:mt-3'>
                 <p className='text-base font-bold text-white'>IDO</p>
                 <div className='bg-[#7FF2521A] md:rounded-[12px] md:px-[6px] md:py-[2px] xl:rounded-[16px] xl:px-[8px] xl:py-[2px] 2xl:rounded-[20px] 2xl:px-[10px] 2xxl:px-[12px] 2xl:py-[3px] 2xxl:py-[4px]'>
-                <p className='font-inter font-medium md:mt-[3px] text-[#7FF252] md:text-[10px] xl:text-[9px] 2xl:text-[10px] 2xxl:text-[12px]'>launchpad</p>
+                <p className='font-inter font-medium md:mt-[3px]  text-[#7FF252] md:text-[10px] xl:text-[9px] 2xl:text-[10px] 2xxl:text-[12px]'>launchpad</p>
 
                 </div>
 
@@ -28,15 +28,15 @@ const webGaming = () => {
             
             
             
-            <p className="font-inter  tracking-wide text-[#C8C8C8] line-clamp md:text-[10px] ml-4 md:pb-4 xl:text-[12px] 2xl:text-[12px] 2xxl:text-base">Access GameFi incubations and
+            <p className="font-inter  tracking-wide text-[#C8C8C8] line-clamp md:text-[10px] ml-4  md:pb-4 xl:text-[12px] 2xl:text-[12px] 2xxl:text-base">Access GameFi incubations and
 other top-tier tokens and NFT
-projects before they reach</p>
+projects before</p>
 </div>
         </div>
        
 
         
-        <div className='  mt-8 md:mt-0 flex xs:w-[330px] xs:h-[160px] cursor-pointer pl-2 pt-2 pr-2 flex-col pb-8 justify-center rounded-[24px] bg-[#040405] hover:bg-[#272727] md:h-[160px]  md:gap-2 xl:h-[170px] xl:w-[17.1875vw] xl:gap-3 2xl:h-[210px] 2xl:gap-4 2xxl:h-[220px]'>
+        <div className='  mt-8 md:mt-0 flex xs:w-[330px] xs:h-[160px] xs:pb-10 cursor-pointer pl-2 pt-2 pr-2 flex-col pb-8 justify-center rounded-[24px] bg-[#040405] hover:bg-[#272727] md:h-[160px]  md:gap-2 xl:h-[170px] xl:w-[17.1875vw] xl:gap-3 2xl:h-[210px] 2xl:gap-4 2xxl:h-[220px]'>
         {/* <img src='assets/Web3/igo.20418964.svg' className='sm:hidden'/> */}
             <div className=' sm:block '>
             <img src='/assets/Web3/ido.802696b8.svg' className='md:w-[18%]  xs:ml-4 mt-8 md:ml-3'/>
@@ -83,7 +83,7 @@ projects before they reach</p>
         </div>
         </div>
 
-        <div className='  mt-8 md:mt-0 flex xs:w-[330px] xs:h-[160px]  cursor-pointer  pl-2 pt-2 pr-2 flex-col pb-8 justify-center rounded-[24px] bg-[#040405] hover:bg-[#272727] md:h-[160px] md:gap-2 xl:h-[170px] xl:w-[17.1875vw] xl:gap-3 2xl:h-[210px] 2xl:gap-4 2xxl:h-[220px]'>
+        <div className='  mt-8 md:mt-0 flex xs:w-[330px] xs:h-[160px]  sx:pb-4  cursor-pointer  pl-2 pt-2 pr-2 flex-col pb-8 justify-center rounded-[24px] bg-[#040405] hover:bg-[#272727] md:h-[160px] md:gap-2 xl:h-[170px] xl:w-[17.1875vw] xl:gap-3 2xl:h-[210px] 2xl:gap-4 2xxl:h-[220px]'>
              {/* <img src='assets/Web3/igo.20418964.svg' className='  sm:hidden'/> */}
             <div className=' sm:block '>
             <img src='/assets/Web3/dmission.74c5d322.svg' className='md:w-[18%] xs:ml-4  mt-10 md:ml-3'/>
