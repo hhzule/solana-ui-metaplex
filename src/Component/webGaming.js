6b482e1ac9a9dae5d7437e05e8f3780d9023fd87
unknown <zubairwastibaloch@gmail.com>
2024-01-20 13:29:28 +0500

    new text added in webgaming section

--- src/Component/webGaming.js
+++ src/Component/webGaming.js
@@ -10,12 +10,12 @@ const webGaming = () => {
     </div>
     <div className='sm:flex  sm:flex-wrap sm:gap-6 sm-mt-20 md:flex !md:flex-wrap md:gap-6 md:mt-20'>
     
-        <div className='mt-8  md:mt-0 md:flex w-[343px]  cursor-pointer  pl-2 pt-2  flex-col pb-8 justify-center rounded-[24px] bg-[#272727] hover:bg-[#040405] md:h-[160px]
+        <div className='mt-8  md:mt-0 md:flex xs:w-[330px] sx:h-[150px] cursor-pointer  pl-2 pt-2  flex-col pb-8 justify-center rounded-[24px] bg-[#272727] hover:bg-[#040405] md:h-[160px]
           md:gap-2 xl:h-[170px] xl:w-[17.1875vw] xl:gap-3 2xl:h-[210px] 2xl:gap-4 2xxl:h-[220px]'>
-            <img src='assets/Web3/igo.20418964.svg' className='sm:hidden'/>
-            <div className='hidden sm:block '>
-            <img src='/assets/Web3/ido.802696b8.svg' className='md:w-[18%] mt-8 md:ml-3'/>
-            <div className='flex gap-2 md:ml-4 md:mt-3'>
+            {/* <img src='assets/Web3/igo.20418964.svg' className='sm:hidden'/> */}
+            <div className=' sm:block '>
+            <img src='/assets/Web3/ido.802696b8.svg' className='md:w-[18%] xs:ml-4 mt-8 md:ml-3'/>
+            <div className='flex xs:ml-4 gap-2 md:ml-4 md:mt-3'>
                 <p className='text-base font-bold text-white'>IDO</p>
                 <div className='bg-[#7FF2521A] md:rounded-[12px] md:px-[6px] md:py-[2px] xl:rounded-[16px] xl:px-[8px] xl:py-[2px] 2xl:rounded-[20px] 2xl:px-[10px] 2xxl:px-[12px] 2xl:py-[3px] 2xxl:py-[4px]'>
                 <p className='font-inter font-medium md:mt-[3px] text-[#7FF252] md:text-[10px] xl:text-[9px] 2xl:text-[10px] 2xxl:text-[12px]'>launchpad</p>
@@ -36,11 +36,11 @@ projects before they reach</p>
        
 
         
-        <div className='  mt-8 md:mt-0 flex w-[343px] cursor-pointer pl-2 pt-2 pr-2 flex-col pb-8 justify-center rounded-[24px] bg-[#040405] hover:bg-[#272727] md:h-[160px]  md:gap-2 xl:h-[170px] xl:w-[17.1875vw] xl:gap-3 2xl:h-[210px] 2xl:gap-4 2xxl:h-[220px]'>
-        <img src='assets/Web3/igo.20418964.svg' className='sm:hidden'/>
-            <div className='hidden sm:block '>
-            <img src='/assets/Web3/ido.802696b8.svg' className='md:w-[18%] mt-8 md:ml-3'/>
-            <div className='flex gap-2 md:mt-2 md:ml-4'>
+        <div className='  mt-8 md:mt-0 flex xs:w-[330px] sx:h-[150px] cursor-pointer pl-2 pt-2 pr-2 flex-col pb-8 justify-center rounded-[24px] bg-[#040405] hover:bg-[#272727] md:h-[160px]  md:gap-2 xl:h-[170px] xl:w-[17.1875vw] xl:gap-3 2xl:h-[210px] 2xl:gap-4 2xxl:h-[220px]'>
+        {/* <img src='assets/Web3/igo.20418964.svg' className='sm:hidden'/> */}
+            <div className=' sm:block '>
+            <img src='/assets/Web3/ido.802696b8.svg' className='md:w-[18%]  xs:ml-4 mt-8 md:ml-3'/>
+            <div className='flex gap-2 md:mt-2  xs:ml-4 md:ml-4'>
                 <p className='text-base font-bold text-white'>Swiping</p>
                 <div className='bg-[#7FF2521A] md:rounded-[12px] md:px-[6px] md:py-[2px] xl:rounded-[16px] xl:px-[8px] xl:py-[2px] 2xl:rounded-[20px] 2xl:px-[10px] 2xxl:px-[12px] 2xl:py-[3px] 2xxl:py-[4px]'>
                 <p className='font-inter font-medium md:mt-[3px] text-[#7FF252] md:text-[10px] xl:text-[9px] 2xl:text-[10px] 2xxl:text-[12px]'>launchpad</p>
@@ -60,11 +60,11 @@ projects before they reach</p>
         </div>
 
 
-        <div className='  mt-8 md:mt-0 flex w-[343px] cursor-pointer pl-2 pt-2 pr-2 flex-col pb-8 justify-center rounded-[24px] bg-[#272727] hover:bg-[#040405] md:h-[160px]  md:gap-2 xl:h-[170px] xl:w-[17.1875vw] xl:gap-3 2xl:h-[210px] 2xl:gap-4 2xxl:h-[220px]'>
-            <img src='assets/Web3/igo.20418964.svg' className='sm:hidden'/>
-            <div className='hidden sm:block '>
-            <img src='/assets/Web3/nft.b3c811f3.svg' className='md:w-[18%] mt-8 md:ml-3'/>
-            <div className='flex gap-2 md:ml-4 md:mt-3'>
+        <div className='  mt-8 md:mt-0 flex xs:w-[330px] sx:h-[150px] cursor-pointer pl-2 pt-2 pr-2 flex-col pb-8 justify-center rounded-[24px] bg-[#272727] hover:bg-[#040405] md:h-[160px]  md:gap-2 xl:h-[170px] xl:w-[17.1875vw] xl:gap-3 2xl:h-[210px] 2xl:gap-4 2xxl:h-[220px]'>
+            {/* <img src='assets/Web3/igo.20418964.svg' className='sm:hidden'/> */}
+            <div className=' sm:block '>
+            <img src='/assets/Web3/nft.b3c811f3.svg' className='md:w-[18%] xs:ml-4 mt-8 md:ml-3'/>
+            <div className='flex gap-2 xs:ml-4  md:ml-4 md:mt-3'>
                 <p className='text-base font-bold text-white'>Staking</p>
                 <div className='bg-[#7FF2521A] md:rounded-[12px] md:px-[6px] md:py-[2px] xl:rounded-[16px] xl:px-[8px] xl:py-[2px] 2xl:rounded-[20px] 2xl:px-[10px] 2xxl:px-[12px] 2xl:py-[3px] 2xxl:py-[4px]'>
                 <p className='font-inter font-medium md:mt-[3px] text-[#7FF252] md:text-[10px] xl:text-[9px] 2xl:text-[10px] 2xxl:text-[12px]'>launchpad</p>
@@ -83,11 +83,11 @@ projects before they reach</p>
         </div>
         </div>
 
-        <div className='  mt-8 md:mt-0 flex w-[343px] cursor-pointer  pl-2 pt-2 pr-2 flex-col pb-8 justify-center rounded-[24px] bg-[#040405] hover:bg-[#272727] md:h-[160px] md:gap-2 xl:h-[170px] xl:w-[17.1875vw] xl:gap-3 2xl:h-[210px] 2xl:gap-4 2xxl:h-[220px]'>
-             <img src='assets/Web3/igo.20418964.svg' className='  sm:hidden'/>
-            <div className='hidden sm:block '>
-            <img src='/assets/Web3/dmission.74c5d322.svg' className='md:w-[18%] mt-10 md:ml-3'/>
-            <div className='flex gap-2 md:ml-4'>
+        <div className='  mt-8 md:mt-0 flex xs:w-[330px] sx:h-[150px] cursor-pointer  pl-2 pt-2 pr-2 flex-col pb-8 justify-center rounded-[24px] bg-[#040405] hover:bg-[#272727] md:h-[160px] md:gap-2 xl:h-[170px] xl:w-[17.1875vw] xl:gap-3 2xl:h-[210px] 2xl:gap-4 2xxl:h-[220px]'>
+             {/* <img src='assets/Web3/igo.20418964.svg' className='  sm:hidden'/> */}
+            <div className=' sm:block '>
+            <img src='/assets/Web3/dmission.74c5d322.svg' className='md:w-[18%] xs:ml-4  mt-10 md:ml-3'/>
+            <div className='flex xs:ml-4 gap-2 md:ml-4'>
                 <p className='text-base font-bold text-white'>Presale</p>
                 <div className='bg-[#7FF2521A] md:rounded-[12px] md:px-[6px] md:py-[2px] xl:rounded-[16px] xl:px-[8px] xl:py-[2px] 2xl:rounded-[20px] 2xl:px-[10px] 2xxl:px-[12px] 2xl:py-[3px] 2xxl:py-[4px]'>
                 <p className='font-inter font-medium md:mt-[3px] text-[#7FF252] md:text-[10px] xl:text-[9px] 2xl:text-[10px] 2xxl:text-[12px]'>launchpad</p>
